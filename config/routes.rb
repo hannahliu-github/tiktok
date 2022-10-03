30c74b6231fa93e5e8290603e1833f1d6e66749e
hannahliu-github <shhan.liu@gmail.com>
2022-10-02 22:21:27 -0400

    Initialize Videos Controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'static_pages/index'
   root 'static_pages#index'
+
+  # Create a folder called API under controllers
+  namespace :api do
+    resources :videos, only: [:index, :show]
+  end
 end
