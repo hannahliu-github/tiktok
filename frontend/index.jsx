a88ff17ea47ae5010e9ca59ebde3ecabb4395d39
hannahliu-github <shhan.liu@gmail.com>
2022-10-09 13:24:25 -0400

    Enable YouTube search and embed videos.

--- frontend/index.jsx
+++ frontend/index.jsx
@@ -5,16 +5,21 @@ import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
 import InputGroup from 'react-bootstrap/InputGroup';
+import ListGroup from 'react-bootstrap/ListGroup';
 import Row from 'react-bootstrap/Row';
 
 import './style.css';
+
+const YOUTUBE_ENDPOINT = "https://www.googleapis.com/youtube/v3/search";
+const API_KEY = "AIzaSyDbTL3fUHn1W82wOsbN_Gpjg6o0USfwYZk";
 
 class Index extends React.Component {
     constructor(props) {
         super(props)
 
         this.state = {
-            searchQuery: ""
+            searchQuery: "",
+            videoIds: []
         }
 
         this.handleInput = this.handleInput.bind(this);
@@ -31,9 +36,17 @@ class Index extends React.Component {
     onSearchClick() {
         console.log('clicked search button for ' + this.state.searchQuery);
 
-        this.setState({
-            searchQuery: ""
-        });
+        // Call YouTube search API
+        const searchUrl = YOUTUBE_ENDPOINT + '?' + 'key=' + API_KEY + '&part=snippet&maxResults=20&q=' + this.state.searchQuery;
+        console.log(searchUrl);
+        fetch(searchUrl)
+            .then(resp => resp.json())
+            .then(videos => this.setState({
+                    searchQuery: "",
+                    videoIds: videos.items.map(video => video.id.videoId)
+                })
+            );
+
     }
 
     render() {
@@ -65,6 +78,20 @@ class Index extends React.Component {
                 </Row>
 
                 <h1 id="customHeading">style.css connected</h1>
+                <ListGroup>
+                    {this.state.videoIds.map(videoId => (
+                        <ListGroup.Item
+                            key={videoId}
+                        >
+                        <iframe
+                            width="100%"
+                            height="100%"
+                            src={"https://www.youtube.com/embed/" + videoId + "?autoplay=0&frameborder=0"}
+                            allow="accelerometer; autoplay; clipboard-write; encrypted-media;
+                                gyroscope; picture-in-picture" allowFullScreen />
+                        </ListGroup.Item>)
+                    )}
+                </ListGroup>
             </Container>
         );
     }
